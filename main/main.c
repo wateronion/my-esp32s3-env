73e4e5e677082d3bf5a350fd0c2e7bef1c7e2b69
admin <wangyueheng@golway.com>
2026-03-31 14:49:49 +0800

    v1.10 创建一个自定义的基础控件，带阴影

--- main/main.c
+++ main/main.c
@@ -29,22 +29,22 @@ void app_main(void)
 
     if (esp_lv_adapter_lock(-1) == ESP_OK)
     {
-        // lv_obj_t * obj1;
-        // obj1 = lv_obj_create(lv_screen_active());
-        // lv_obj_set_size(obj1, 100, 50);
-        // lv_obj_align(obj1, LV_ALIGN_CENTER, -60, -30);
+        lv_obj_t * obj1;
+        obj1 = lv_obj_create(lv_screen_active());
+        lv_obj_set_size(obj1, 100, 50);
+        lv_obj_align(obj1, LV_ALIGN_CENTER, -60, -30);
 
-        // static lv_style_t style_shadow;
-        // lv_style_init(&style_shadow);
-        // lv_style_set_shadow_width(&style_shadow, 10);
-        // lv_style_set_shadow_spread(&style_shadow, 5);
-        // lv_style_set_shadow_color(&style_shadow, lv_palette_main(LV_PALETTE_BLUE));
+        static lv_style_t style_shadow;
+        lv_style_init(&style_shadow);
+        lv_style_set_shadow_width(&style_shadow, 10);
+        lv_style_set_shadow_spread(&style_shadow, 5);
+        lv_style_set_shadow_color(&style_shadow, lv_palette_main(LV_PALETTE_BLUE));
 
-        // lv_obj_t * obj2;
-        // obj2 = lv_obj_create(lv_screen_active());
-        // lv_obj_add_style(obj2, &style_shadow, 0);
-        // lv_obj_align(obj2, LV_ALIGN_CENTER, 60, 30);
-        lv_demo_benchmark();
+        lv_obj_t * obj2;
+        obj2 = lv_obj_create(lv_screen_active());
+        lv_obj_add_style(obj2, &style_shadow, 0);
+        lv_obj_align(obj2, LV_ALIGN_CENTER, 60, 30);
+        // lv_demo_benchmark();
         esp_lv_adapter_unlock();
     }
 
